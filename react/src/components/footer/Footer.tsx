708cde8e03b4f00d1c36d63ffa913175bcaa3026
MordilloSan <miguelgalizamariz@gmail.com>
2025-06-09 11:54:53 +0100

    update

--- react/src/components/footer/Footer.tsx
+++ react/src/components/footer/Footer.tsx
@@ -1,11 +1,4 @@
-import {
-  Grid,
-  List,
-  ListItemText,
-  ListItemButton,
-  Box,
-  useTheme,
-} from "@mui/material";
+import { Grid, Box, useTheme } from "@mui/material";
 import React from "react";
 
 function Footer() {
@@ -16,8 +9,7 @@ function Footer() {
       sx={{
         background: theme.footer?.background || theme.palette.background.paper,
         position: "relative",
-      }}
-    >
+      }}>
       <Grid container spacing={0}>
         {/* Left side links */}
         <Grid
@@ -25,36 +17,7 @@ function Footer() {
             xs: 12,
             md: 6,
           }}
-          sx={{ display: { xs: "none", md: "block" } }}
-        >
-          <List>
-            {["Support", "Help Center", "Privacy", "Terms of Service"].map(
-              (text) => (
-                <ListItemButton
-                  key={text}
-                  component="a"
-                  href="#"
-                  sx={{
-                    display: "inline-block",
-                    width: "auto",
-                    px: 2,
-                    color: "#ff0000",
-                    "&:hover, &:active": theme.footer.color,
-                  }}
-                >
-                  <ListItemText
-                    primary={text}
-                    slotProps={{
-                      primary: {
-                        sx: { color: theme.footer?.color || "text.primary" },
-                      },
-                    }}
-                  />
-                </ListItemButton>
-              ),
-            )}
-          </List>
-        </Grid>
+          sx={{ display: { xs: "none", md: "block" } }}></Grid>
 
         {/* Right side copyright */}
         <Grid
@@ -63,29 +26,7 @@ function Footer() {
             md: 6,
           }}
           container
-          justifyContent="flex-end"
-        >
-          <List>
-            <ListItemButton
-              sx={{
-                display: "inline-block",
-                width: "auto",
-                px: 2,
-                color: "#ff0000",
-                "&:hover, &:active": { color: "#ff0000" },
-              }}
-            >
-              <ListItemText
-                primary={`© ${new Date().getFullYear()} - Mira`}
-                slotProps={{
-                  primary: {
-                    sx: { color: theme.footer?.color || "text.primary" },
-                  },
-                }}
-              />
-            </ListItemButton>
-          </List>
-        </Grid>
+          justifyContent="flex-end"></Grid>
       </Grid>
     </Box>
   );
